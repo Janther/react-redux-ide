9446dc7a55400a0170bab7878843e8292a33b7c3
Klaus Hott Vidal <klahott@gmail.com>
2017-06-04 17:18:51 +1200

    using styled-components on highlights

--- src/editor/highlights/Highlights.js
+++ src/editor/highlights/Highlights.js
@@ -2,17 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import styled from 'styled-components';
 
-const Highlights = ({}) => {
-  return (
-    <div className={classNames('highlights')}>
-      <div className={classNames('highlight', 'selection')}>
-        <div className={classNames('region')} style={{boxSizing: 'border-box', top: '0px',  left: '0px', right: '0px',  height: '21px'}}></div>
-        <div className={classNames('region')} style={{boxSizing: 'border-box', top: '21px', left: '0px', width: '25px', height: '21px'}}></div>
-      </div>
-    </div>
-  )
-}
+const StyledRegion = styled.div.attrs({
+  className: classNames('region'),
+})`
+  ${'' /* animation-name: flash;
+  animation-duration: .5s;
+  animation-iteration-count: 1; */}
+`
+
+const StyledHighlight = styled.div.attrs({
+  className: classNames('highlight', 'selection')
+})`
+  background: none;
+  border-radius: 2px;
+  color: #d7dae0;
+  font-weight: 700;
+  padding: 0;
+  ${StyledRegion} {
+    position: absolute;
+    pointer-events: none;
+    z-index: -1;
+  }
+`;
+
+const Highlights = () => (
+  <div className={classNames('highlights')}>
+    <StyledHighlight>
+      <StyledRegion style={{boxSizing: 'border-box', top: '0px',  left: '0px', right: '0px',  height: '21px'}}></StyledRegion>
+      <StyledRegion style={{boxSizing: 'border-box', top: '21px', left: '0px', width: '25px', height: '21px'}}></StyledRegion>
+    </StyledHighlight>
+  </div>
+)
 
 Highlights.propTypes = {
 }
